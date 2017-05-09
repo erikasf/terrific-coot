cf10451143f4729c21385c42b0048658242b9b8a
erika <erikasf@users.noreply.github.com>
2017-05-09 14:28:14 -0700

    login in oauth api calls to firebase

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBAqhwVZLKjWehPw3Qhpn4wOHJI3vWgH1Y",
+    authDomain: "hoorno-8f93c.firebaseapp.com",
+    databaseURL: "https://hoorno-8f93c.firebaseio.com",
+    projectId: "hoorno-8f93c",
+    storageBucket: "hoorno-8f93c.appspot.com",
+    messagingSenderId: "238979784539"
+  };
+
+  firebase.initializeApp(config);
